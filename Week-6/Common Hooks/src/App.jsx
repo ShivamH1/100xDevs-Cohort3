af35059233e50439d2d41b70da02f50f7a2d5156
ShivamH1 <shivamhonra@gmail.com>
2024-07-09 08:20:07 +0530

    memo

--- Week-6/Common Hooks/src/App.jsx
+++ Week-6/Common Hooks/src/App.jsx
@@ -1,25 +1,98 @@
-import { useState } from "react";
-// import { useEffect } from "react";
-import { Todo } from "../components/Todo";
+import { memo, useState } from "react";
 
 function App() {
-  const [selectedButtonId, setSelectedButtonId] = useState(1);
+  const [count, setCount] = useState(0);
 
-  const handleButtonPress = (buttonId) => {
-    setSelectedButtonId(buttonId);
-  };
+  function logSomething() {
+    console.log("child clicked");
+  }
 
   return (
     <div>
-      <button onClick={() => handleButtonPress(1)}>1</button>
-      <button onClick={() => handleButtonPress(2)}>2</button>
-      <button onClick={() => handleButtonPress(3)}>3</button>
-      <button onClick={() => handleButtonPress(4)}>4</button>
-      <Todo id={selectedButtonId} />
+      <Child onClick={logSomething} />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click me {count}
+      </button>
     </div>
   );
 }
 
+const Child = memo(({ onClick }) => {
+  console.log("child rendered");
+  return (
+    <div>
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+});
+
+// import { useEffect, useMemo, useState } from "react";
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+//   const [inputVal, setInputVal] = useState(1);
+//   // const [finalVal, setFinalVal] = useState(0);
+
+//   const count = useMemo(() => {
+//     let finalCount = 0;
+//     for (let i = 1; i <= inputVal; i++) {
+//       finalCount += i;
+//     }
+//     return finalCount;
+//   }, [inputVal]);
+
+//   // useEffect(() => {
+//   //   let count = 0;
+//   //   for (let i = 1; i <= inputVal; i++) {
+//   //     count += i;
+//   //   }
+//   //   setFinalVal(count);
+//   // }, [inputVal]);
+
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         onChange={(e) => setInputVal(e.target.value)}
+//         placeholder="Enter a value"
+//       ></input>
+//       <p>
+//         Sum for {inputVal} is {count}
+//         {/* Sum for {inputVal} is {finalVal} */}
+//       </p>
+//       <button onClick={() => setCounter(counter + 1)}>Counter {counter}</button>
+//     </div>
+//   );
+// }
+
+// import { useState } from "react";
+// // import { useEffect } from "react";
+// import { Todo } from "../components/Todo";
+
+// function App() {
+//   const [selectedButtonId, setSelectedButtonId] = useState(1);
+
+//   const handleButtonPress = (buttonId) => {
+//     setSelectedButtonId(buttonId);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={() => handleButtonPress(1)}>1</button>
+//       <button onClick={() => handleButtonPress(2)}>2</button>
+//       <button onClick={() => handleButtonPress(3)}>3</button>
+//       <button onClick={() => handleButtonPress(4)}>4</button>
+//       <Todo id={selectedButtonId} />
+//     </div>
+//   );
+// }
+
+// import { useState } from "react";
+// import { Todo } from "../components/Todo";
 // function App() {
 //   const [todos, setTodos] = useState([]);
 
