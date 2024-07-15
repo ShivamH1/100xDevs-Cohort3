afb5891eab1551ffae5554a181934898dd3d4a84
ShivamH1 <shivamhonra@gmail.com>
2024-07-15 17:05:05 +0530

    Week 7

---
+++ Week-7/Routing/components/Landing.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Landing() {
+    return (
+        <>
+            <h1>Landing Page</h1>
+        </>
+    )
+}
---
+++ Week-7/Routing/src/App.jsx
@@ -0,0 +1,49 @@
+import { lazy } from "react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+
+const Landing = lazy(() => import("../components/Landing")); 
+const Dashboard = lazy(() => import("../components/Dashboard"));
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Appbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function Appbar() { 
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Landing Page
+        </button>
+        <button
+          onClick={() => {
+            navigate("/dashboard");
+          }}
+        >
+          Dashboard
+        </button>
+
+        {/* <button onClick={() => window.location.href = "/"}>Landing</button>
+        <button onClick={() => window.location.href = "/dashboard"}>Dashboard</button>       */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
