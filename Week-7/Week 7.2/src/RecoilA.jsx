d7f73dce64b1f4a96cc2f7b646c7cbe67a8a4bb3
ShivamH1 <shivamhonra@gmail.com>
2024-07-17 08:34:34 +0530

    Recoil state management

---
+++ Week-7/Week 7.2/src/RecoilA.jsx
@@ -0,0 +1,53 @@
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { countAtom } from "../store/atoms/count";
+
+function App() {
+  return (
+    <div>
+      <RecoilRoot>
+        <Count></Count>
+      </RecoilRoot>
+    </div>
+  );
+}
+
+function Count() {
+  console.log("count re-rendered");
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return <div>{count}</div>;
+}
+
+function Buttons() {
+  const [count, setCount] = useRecoilState(countAtom);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
--- Week-7/Week 7.2/src/main.jsx
+++ Week-7/Week 7.2/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
+import RecoilA from './RecoilA'
 // import A from './A';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RecoilA />
     {/* <A /> */}
-    <App></App>
+    {/* <App></App> */}
   </React.StrictMode>,
 )
