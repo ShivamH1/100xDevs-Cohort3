d6cb50d365f7ddfa9380094ad65785fdcf11bd98
ShivamH1 <shivamhonra@gmail.com>
2024-07-19 22:51:24 +0530

    Practicing

---
+++ React Practice/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// import Two from '../components/Two'
+// import Three from '../components/Three'
+// import Four from '../components/Four'
+// import Five from '../components/Five'
+// import Six from '../components/Six'
+
+export default function App() {
+  return (
+    <div>
+      {/* <Two /> */}
+      {/* <Three />
+        <Four />
+        <Five />
+        <Six /> */}
+
+    
+    </div>
+  );
+}
