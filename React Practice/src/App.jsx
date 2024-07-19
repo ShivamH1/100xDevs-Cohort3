484b06213e76c3a35eeef463db1227e04fc28336
ShivamH1 <shivamhonra@gmail.com>
2024-07-20 00:21:39 +0530

    Practicing

--- React Practice/src/App.jsx
+++ React Practice/src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./App.css";
+
 // import Two from '../components/Two'
 // import Three from '../components/Three'
 // import Four from '../components/Four'
 // import Five from '../components/Five'
 // import Six from '../components/Six'
+// import Seven from "../components/Seven";
+// import Eight from "../components/Eight";
+// import Nine from "../components/Nine";
+import Ten from "../components/Ten";
 
 export default function App() {
   return (
@@ -13,8 +19,10 @@ export default function App() {
         <Four />
         <Five />
         <Six /> */}
-
-    
+      {/* <Seven /> */}
+      {/* <Eight /> */}
+      {/* <Nine /> */}
+      <Ten />
     </div>
   );
 }
