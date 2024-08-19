abcad1a60594ee69780d7cc48e8df44ae7c8aa21
ShivamH1 <shivamhonra@gmail.com>
2024-08-20 00:17:49 +0530

    Web2

--- Todo/frontend/src/App.jsx
+++ Todo/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { CreateTodo } from "./components/CreateTodo";
 import { Todos } from "./components/Todos";
+import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
