4e11d169dde3377f62a31acbd41bb3b73d6e95ac
ShivamH1 <shivamhonra@gmail.com>
2024-09-24 21:36:52 +0530

    HTTP

--- Cohort 3/Web 2/HTTP/index.js
+++ Cohort 3/Web 2/HTTP/index.js
@@ -5,19 +5,128 @@ app.use(express.json());
 
 const users = [];
 
+function generateToken() {
+  let options = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+
+  let token = "";
+  for (let i = 0; i < 32; i++) {
+    // use a simple function here
+    token += options[Math.floor(Math.random() * options.length)];
+  }
+  return token;
+}
+
+app.get("/me", (req, res) => {
+  const token = req.headers.authorization;
+  const user = users.find((u) => u.token === token);
+  if (user) {
+    res.send({
+      username: user.username,
+    });
+  } else {
+    res.status(403).send({
+      msg: "Invalid credentials",
+    });
+  }
+});
+
 app.post("/signup", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+  const username = req.body.username;
+  const password = req.body.password;
 
-    users.push({username, password});
+  users.push({ username, password });
 
-    res.json({
-        msg : "User created successfully"
-    })
-})
+  res.json({
+    msg: "User created successfully",
+  });
+  console.log(users);
+});
 
 app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-})
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (user) {
+    const token = generateToken();
+    user.token = token;
+    res.json({
+      msg: "User logged in successfully",
+      token: generateToken(),
+    });
+    console.log(users);
+  } else {
+    res.status(403).send({
+      msg: "Invalid credentials",
+    });
+  }
+  console.log(users);
+});
 
 app.listen(3000);
