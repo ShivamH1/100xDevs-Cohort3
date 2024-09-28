0f75c772cd7242b603fd09be197cbe7fce7390cf
ShivamH1 <shivamhonra@gmail.com>
2024-09-28 13:38:08 +0530

    App

--- Cohort 3/Web 2/Course Selling Website/backend/index.js
+++ Cohort 3/Web 2/Course Selling Website/backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/user", userRouter);
-app.use("/courses", courseRoute);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/courses", courseRoute);
 
 app.listen(3000);
--- Cohort 3/Web 2/Course Selling Website/backend/routes/courses.js
+++ Cohort 3/Web 2/Course Selling Website/backend/routes/courses.js
@@ -1,12 +1,16 @@
 const { Router } = require("express");
 const courseRoute = Router();
 
-courseRoute.get("/list", (req, res) => {
-  res.json({});
+courseRoute.get("/preview", (req, res) => {
+  res.json({
+    msg : "preview endpoint"
+  });
 });
 
 courseRoute.get("/purchases", (req, res) => {
-  res.json({});
+  res.json({
+    msg : "purchases endpoint"
+  });
 });
 
 module.exports = {
