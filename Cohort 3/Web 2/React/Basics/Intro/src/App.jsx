6d4f26816302d5ea99d44728f4e2790433cd6d3c
ShivamH1 <shivamhonra@gmail.com>
2024-10-19 23:49:32 +0530

    React Basics

--- Cohort 3/Web 2/React/Basics/Intro/src/App.jsx
+++ Cohort 3/Web 2/React/Basics/Intro/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -62,6 +60,7 @@ const Timer = () => {
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
+      //when mount
       const interval = setInterval(() => {
           setSeconds(prev => prev + 1);
       }, 1000);
