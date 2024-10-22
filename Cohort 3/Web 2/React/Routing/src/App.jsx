79baecabdf21e67ea035efd5b74bf5a13fc8093a
ShivamH1 <shivamhonra@gmail.com>
2024-10-22 22:18:46 +0530

    Single Page Application

---
+++ Cohort 3/Web 2/React/Routing/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//Single Page Application
+// Single Page Applications (SPAs) are web applications that load a single HTML page and dynamically update that page as the user 
+// interacts with the app. This approach allows for a smoother user experience compared to traditional multi-page applications (MPAs),
+// where each interaction often requires a full page reload.
+// How you do that is by using react-routing:
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
