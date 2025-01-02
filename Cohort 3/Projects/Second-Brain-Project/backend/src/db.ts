f967e8b6c9d0e0dda3eddc04ba1057f092ccb1ab
Shivam <shivamhonra@gmail.com>
2025-01-02 21:51:40 +0530

    Middleware for signin, api for content

--- Cohort 3/Projects/Second-Brain-Project/backend/src/db.ts
+++ Cohort 3/Projects/Second-Brain-Project/backend/src/db.ts
@@ -1,12 +1,22 @@
 import mongoose, { model, Schema } from "mongoose";
 
-mongoose.connect(
-    "mongodb+srv://admin:India%40123@100xdev.7gly1js.mongodb.net/brainly"
-  );
+mongoose.connect("mongodb+srv://admin:India%40123@100xdev.7gly1js.mongodb.net/brainly")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("Error connecting to MongoDB:", err));
 
 const UserSchema = new Schema({
-    username: {type: String, unique: true},
-    password: String
-})
+  username: { type: String, unique: true },
+  password: String,
+});
 
 export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+  type: String,
+  link : String,
+  title : String,
+  tags : [{ type : mongoose.Types.ObjectId, ref : 'Tags'}],
+  userId : { type : mongoose.Types.ObjectId, ref : 'User'}
+})
+
+export const ContentModel = model("Content", ContentSchema);
--- Cohort 3/Projects/Second-Brain-Project/backend/src/index.ts
+++ Cohort 3/Projects/Second-Brain-Project/backend/src/index.ts
@@ -3,26 +3,31 @@ import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import { z } from "zod";
 import bcrypt from "bcrypt";
-import { UserModel } from "./db";
+import { ContentModel, UserModel } from "./db";
+import { JWT_PASS } from "./config";
+import { userMiddleware } from "./middleware";
 
 const app = express();
 
 app.use(express.json());
 
 app.post("/api/v1/signup", async (req, res) => {
-  const { username, password } = req.body;
+  const signupSchema = z.object({
+    username: z.string().min(1),
+    password: z.string().min(4),
+  });
 
+  const { username, password } = signupSchema.parse(req.body);
+
+  const hashedPassword = await bcrypt.hash(password, 12);
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await UserModel.create({
       username,
-      password: hashedPassword
+      password: hashedPassword,
     });
-
     res.json({
       message: "User signed up",
-      _id: user._id,
-      __v: user.__v
+      user,
     });
   } catch (e) {
     res.status(411).json({
@@ -31,15 +36,94 @@ app.post("/api/v1/signup", async (req, res) => {
   }
 });
 
-app.post("/api/v1/signin", (req, res) => {
-  const body = req.body;
+// Zod is not required for signin, but it can be useful for input validation.
+app.post("/api/v1/signin", async (req, res) => {
+  const signinSchema = z.object({
+    username: z.string().min(1),
+    password: z.string().min(4),
+  });
+  const { username, password } = signinSchema.parse(req.body);
+
+  //First check if the user exists
+  const existingUser = await UserModel.findOne({ username });
+  if (existingUser) {
+    //Then check if the password is correct using bcrypt.compare() method.
+    const isPasswordValid = existingUser?.password
+      ? await bcrypt.compare(password, existingUser.password)
+      : false;
+    if (isPasswordValid) {
+      const token = jwt.sign({ id: existingUser._id }, JWT_PASS);
+      res.json({
+        message: "User signed in",
+        token,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid username or password" });
+    }
+  } else {
+    res.status(401).json({ message: "Invalid username or password" });
+  }
 });
 
-app.post("/api/v1/content", (req, res) => {});
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+  const { type, link, title } = req.body;
 
-app.get("/api/v1/content", (req, res) => {});
+  try {
+    const content = await ContentModel.create({
+      type,
+      link,
+      title,
+      //@ts-ignore
+      userId: req.userId,
+      tags: [],
+    });
 
-app.delete("/api/v1/content", (req, res) => {});
+    res.json({
+      message: "Content Addded",
+      content,
+    });
+  } catch (e) {
+    res.status(400).json({ message: "Failed to add the content" });
+  }
+});
+
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+  //@ts-ignore
+  const userId = req.userId;
+
+  try {
+    //usage of references (foreign keys)
+    const contents = await ContentModel.find({ userId }).populate(
+      "userId",
+      "username"
+    );
+
+    res.json({
+      message: "Your content: ",
+      contents,
+    });
+  } catch (e) {
+    res.status(400).json({ message: "Failed to get the content" });
+  }
+});
+
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+  const contentId = req.body.contentId;
+
+  try {
+    await ContentModel.deleteMany({
+      contentId,
+      //@ts-ignore
+      userId: req.userId,
+    });
+
+    res.json({
+      message: "Deleted",
+    });
+  } catch (e) {
+    res.status(400).json({ message: "Failed to delete the content" });
+  }
+});
 
 app.post("/api/v1/brain/share", (req, res) => {});
 
