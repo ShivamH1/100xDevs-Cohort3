e5df5b2fbb8eb2d3fa8ad5c14ac37ad8edba5620
Shivam <shivamhonra@gmail.com>
2025-01-30 16:37:48 +0530

    Broadcasting of message, Initialized frontend

--- Cohort 3/Projects/ChatApp/backend/dist/index.js
+++ Cohort 3/Projects/ChatApp/backend/dist/index.js
@@ -3,11 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 let clientCount = 0;
+let allSockets = [];
 wss.on("connection", (socket) => {
+    allSockets.push(socket);
     clientCount++;
     console.log(`client connected #: ${clientCount}`);
     socket.on("message", (message) => {
         console.log(`received message: ${message.toString()}`);
-        socket.send(message.toString() + "sent from server");
+        allSockets.forEach((socket) => {
+            socket.send(message.toString() + " sent from server");
+        });
     });
 });
