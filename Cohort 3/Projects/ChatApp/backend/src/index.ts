ab4345d9dfde6c5e7661776b74c49c5597950781
ShivamH1 <shivamhonra@gmail.com>
2025-02-02 00:25:29 +0530

    Creating custom rooms to chat

--- Cohort 3/Projects/ChatApp/backend/src/index.ts
+++ Cohort 3/Projects/ChatApp/backend/src/index.ts
@@ -4,31 +4,44 @@ const wss = new WebSocketServer({ port: 8080 });
 
 interface User {
   socket: WebSocket;
-  room : string;
+  room: string;
 }
 
 let clientCount = 0;
 let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-
   socket.on("message", (message) => {
     //Here we converted the message/string to object. Message is from the client.
-     const parsedMessage = JSON.parse(message);
-     if (parsedMessage.type === 'join') {
+    const parsedMessage = JSON.parse(message);
+
+    if (parsedMessage.type === "join") {
       allSockets.push({
         socket: socket,
         room: parsedMessage.payload.roomId,
-      })
-     }
+      });
+    }
+
+    if (parsedMessage.type === "chat") {
+      const currentClientRoom = allSockets.find(
+        (x) => x.socket === socket
+      )?.room;
+
+      const socketsInCurrentRoom = allSockets.filter(
+        (s) => s.room === currentClientRoom
+      );
+      socketsInCurrentRoom.forEach((s) =>
+        s.socket.send(parsedMessage.payload.message)
+      );
+    }
   });
 
   socket.on("disconnect", () => {
     // remove the socket that disconnected from the array of all sockets
     // this is done by creating a new array that only includes the sockets
     // that are not equal to the socket that disconnected
-    allSockets = allSockets.filter((s) => s!== socket);
+    allSockets = allSockets.filter((s) => s !== socket);
     clientCount--;
     console.log(`client disconnected: ${clientCount}`);
-  })
+  });
 });
