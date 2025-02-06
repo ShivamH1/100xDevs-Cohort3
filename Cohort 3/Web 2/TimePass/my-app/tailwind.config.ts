5b513531df3493893fd60896b1ff6a4abf545e3e
Shivam <shivamhonra@gmail.com>
2025-02-06 12:32:09 +0530

    Related to learning of Shadcn

--- Cohort 3/Web 2/TimePass/my-app/tailwind.config.ts
+++ Cohort 3/Web 2/TimePass/my-app/tailwind.config.ts
@@ -1,17 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 export default {
     darkMode: ["class"],
     content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -54,6 +51,11 @@ export default {
   				'5': 'hsl(var(--chart-5))'
   			}
   		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
   		keyframes: {
   			'accordion-down': {
   				from: {
@@ -79,5 +81,4 @@ export default {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-}
-
+} satisfies Config;
