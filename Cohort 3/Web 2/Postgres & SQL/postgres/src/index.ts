1be6a48d250ef4ac43d503d283fd8df4e4908cc0
ShivamH1 <shivamhonra@gmail.com>
2025-02-14 23:11:13 +0530

    query in endpoints

--- Cohort 3/Web 2/Postgres & SQL/postgres/src/index.ts
+++ Cohort 3/Web 2/Postgres & SQL/postgres/src/index.ts
@@ -1,4 +1,8 @@
 import { Client } from "pg";
+import express from "express";
+
+const app = express();
+app.use(express.json());
 
 // const pgClient2 = new Client(
 //   "postgresql://neondb_owner:npg_m1icbf0oSukD@ep-misty-star-a1fef2q6-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
@@ -14,10 +18,43 @@ const pgClient = new Client({
 });
 
 //This is a async function
-async function main() {
-  await pgClient.connect();
-  const response = await pgClient.query("SELECT * FROM users;");
-  console.log(response.rows);
-}
+// async function main() {
+//   await pgClient.connect();
+//   const response = await pgClient.query("SELECT * FROM users;");
+//   console.log(response.rows);
+// }
 
-main();
+// main();
+
+pgClient.connect();
+
+app.post("/signup", async (req: express.Request, res: express.Response) => {
+  const { username, password, email, city, country, street, pincode } =
+    req.body;
+  try {
+    const insertQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3);`;
+
+    const response = await pgClient.query(insertQuery, [
+      username,
+      password,
+      email,
+    ]);
+
+    const addressInsertQuery = `INSERT INTO address (city, street, pincode, country, userId) VALUES ($1, $2, $3, $4, $5);`;
+
+    const responseAddress = await pgClient.query(addressInsertQuery, [
+      city,
+      street,
+      pincode,
+      country,
+      response.rows[0].id,
+    ]);
+
+    res.json({
+      message: "success",
+    });
+  } catch (err) {
+    console.error("Error during signup:", err);
+    res.status(500).send("Internal server error");
+  }
+});
