4273664e3d2ec1285e639e65bb3a9239fdbb297b
ShivamH1 <shivamhonra@gmail.com>
2025-02-21 23:13:06 +0530

    Crud operation in prisma.

--- Cohort 3/Web 2/orm-prisma/src/index.ts
+++ Cohort 3/Web 2/orm-prisma/src/index.ts
@@ -1,3 +1,60 @@
 import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
+
+async function createUser() {
+  try {
+    const user = await client.user.create({
+      data: {
+        username: "testuser",
+        password: "testpassword",
+        age: "30",
+        city: "New York",
+      },
+    });
+    console.log("user created", user);
+  } catch (error) {
+    console.error("Error creating user:", error);
+  }
+}
+
+async function getUser() {
+  try {
+    const user = await client.user.findUnique({
+      where: { username: "testuser" },
+    });
+    console.log("user found", user);
+  }catch (error) {
+    console.error("Error finding user:", error);
+  }
+}
+
+async function updateUser() {
+  try {
+    const user = await client.user.update({
+      where: { username: "testuser" },
+      data: { age: "31" },
+    });
+    console.log("user updated", user);
+  } catch (error) {
+    console.error("Error updating user:", error);
+  }
+}
+
+async function deleteUser() {
+  try {
+    const user = await client.user.delete({
+      where: { username: "testuser" },
+    });
+    console.log("user deleted", user);
+  } catch (error) {
+    console.error("Error deleting user:", error);
+  }
+}
+
+async function main() {
+  await createUser();
+  await getUser();
+  await updateUser();
+  await deleteUser();
+}
