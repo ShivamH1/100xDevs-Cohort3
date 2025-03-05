7803f0cd4e30ce9aa25e875da3fc47f43ef9bd6d
Shivam <shivamhonra@gmail.com>
2025-03-05 17:56:23 +0530

    prisma singleton

--- Cohort 3/Web 2/NextJs/SimpleTask/src/app/api/v1/signup/route.ts
+++ Cohort 3/Web 2/NextJs/SimpleTask/src/app/api/v1/signup/route.ts
@@ -1,18 +1,37 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+//singleton prisma client
+import prisma from "@/app/lib/db";
 
-const prismaClient = new PrismaClient();
+// const prismaClient = new PrismaClient();
 
 // export async function GET() {
 //     const users = await prismaClient.user.findMany();
 //     return NextResponse.json({ users });
 // }
 
+// export async function POST(req: NextRequest) {
+//     const data = await req.json();
+//     console.log("data", data);
+
+//     await prismaClient.user.create({
+//         data: {
+//             username: data.username,
+//             password: data.password,
+//         },
+//     })
+    
+//     return NextResponse.json({
+//         message : "You have signed up successfully!",
+//         status : 200
+//     })
+// }
+
 export async function POST(req: NextRequest) {
     const data = await req.json();
     console.log("data", data);
 
-    await prismaClient.user.create({
+    await prisma.user.create({
         data: {
             username: data.username,
             password: data.password,
